6f9d3254b08dc98ba76906929740c44c74fe9148
Fn4ch <farhadmainsot@gmail.com>
2023-02-20 15:47:53 +0300

    1

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosError } from 'axios';
+import axios from 'axios';
 import { HttpService } from '@nestjs/axios/dist';
 import { IGetTokenResp } from './models';
 import { firstValueFrom } from 'rxjs';
@@ -9,69 +9,85 @@ export class AppService {
   constructor(private readonly httpService: HttpService) {}
 
   async getToken(): Promise<IGetTokenResp> {
-    const token = await axios.get<IGetTokenResp>(
-      'https://test.gnzs.ru/oauth/get-token.php',
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'X-Client-Id': '30878566',
+    try {
+      const token = await axios.get<IGetTokenResp>(
+        'https://test.gnzs.ru/oauth/get-token.php',
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'X-Client-Id': '30878566',
+          },
         },
-      },
-    );
-    return token.data;
+      );
+      return token.data;
+    } catch (e) {
+      return e;
+    }
   }
 
   async createDeal(): Promise<any> {
     const { base_domain, access_token } = await this.getToken();
     try {
+      const body: [object] = [
+        {
+          name: 'deal 3000',
+        },
+      ];
       const { data } = await firstValueFrom(
-        this.httpService.post(`https://${base_domain}/api/v4/leads`, {
+        this.httpService.post(`https://${base_domain}/api/v4/leads`, body, {
           headers: {
             Authorization: `Bearer ${access_token}`,
             'Content-Type': 'application/json',
           },
         }),
       );
-      return data;
+      return data['_embedded']['leads'];
     } catch (e) {
-      console.log(e);
-      return e;
+      return e.message;
     }
   }
 
   async createContact() {
     const { base_domain, access_token } = await this.getToken();
     try {
+      const body: [object] = [
+        {
+          name: 'contact 3000',
+        },
+      ];
       const { data } = await firstValueFrom(
-        this.httpService.post(`https://${base_domain}/api/v4/contacts`, {
+        this.httpService.post(`https://${base_domain}/api/v4/contacts`, body, {
           headers: {
             'Content-Type': 'application/json',
             Authorization: `Bearer ${access_token}`,
           },
         }),
       );
-      return data;
+      return data['_embedded']['contacts'];
     } catch (e) {
-      console.log(e);
-      return e;
+      return e.message;
     }
   }
 
   async createCompany() {
     const { base_domain, access_token } = await this.getToken();
     try {
+      const body: [object] = [
+        {
+          name: 'company 3000',
+        },
+      ];
       const { data } = await firstValueFrom(
-        this.httpService.post(`https://${base_domain}/api/v4/companiess`, {
+        this.httpService.post(`https://${base_domain}/api/v4/companies`, body, {
           headers: {
             'Content-Type': 'application/json',
             Authorization: `Bearer ${access_token}`,
           },
         }),
       );
-      return data;
+      return data['_embedded']['companies'];
     } catch (e) {
-      console.log('error:' + e);
-      return e;
+      return e.message;
     }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(3000, () => {
-    console.log('server started on port 3000')
-  })
+    console.log('server started on port 3000');
+  });
 }
-bootstrap()
+bootstrap();
